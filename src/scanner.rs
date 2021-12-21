a3b4af29fe60ab0a8dfcd31adcfa78be2e613a47
Nate Wilson <nsw.wilson@gmail.com>
2021-12-20 20:11:10 -0800

    Finishing chp. 4 -> finalizing scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,6 +1,6 @@
 use crate::token_type::TokenType;
 use crate::token::{Token, Literal};
-use crate::lox::LoxError;
+use crate::lox::{LoxError};
 
 pub struct Scanner {
     source: String,
@@ -84,11 +84,82 @@ impl Scanner {
                     TokenType::Greater
                 });
             },
-            _ch => {
-                return Err(LoxError{line: self.line, message: "Unexpected character.".to_string()})
+            '/' => {
+                let comment = self.match_next('/');
+                if comment {
+                    // A comment goes until the end of the line.
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::Slash);
+                }
+            },
+            ' ' | '\r' | '\t' => (),
+            '\n' => self.line += 1,
+            '"' => self.string()?,
+            ch => {
+                if self.is_digit(ch) {
+                    self.number();
+                } else if self.is_alpha(ch) {
+                    self.identifier()
+                } else {
+                    return Err(LoxError{line: self.line, message: "Unexpected character.".to_string()})
+                } 
             }
         }
         Ok(())
+    }
+
+    fn identifier(&mut self) {
+        while self.is_alpha_numeric(self.peek()) { self.advance(); }
+
+        let text = &self.source[self.start as usize .. self.current as usize];
+        let token_type = self.get_keyword(text);
+        match token_type {
+            Some(t) => self.add_token(t),
+            _ => self.add_token(TokenType::Identifier)
+        }
+    }
+
+    fn number(&mut self) {
+        while self.is_digit(self.peek()) { self.advance(); }
+
+        // Look for a fractional part
+        if self.peek() == '.' && self.is_digit(self.peek_next()) {
+            // Consume the ','
+            self.advance();
+
+            while self.is_digit(self.peek()) { self.advance(); }
+        }
+
+        let num: f64 = self.source[self.start as usize .. self.current as usize]
+                .to_string()
+                .parse()
+                .unwrap();
+
+        self.add_token_literal(TokenType::Number, Literal::Number(num))
+    }
+
+    fn string(&mut self) -> Result<(), LoxError> {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' { self.line += 1; }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            return Err(LoxError{ line: self.line, message: "Unterminated string.".to_string() })
+        }
+
+        // The closing "
+        self.advance();
+
+        let value = self.source[(self.start+1) as usize .. (self.current-1) as usize].to_string();
+
+        self.add_token_literal(TokenType::String, Literal::String(value));
+
+        Ok(())
+
     }
 
     fn match_next(&mut self, expected: char) -> bool {
@@ -99,8 +170,36 @@ impl Scanner {
         true
     }
 
+    fn peek(&self) -> char {
+        if self.is_at_end() {  
+            return '\0';
+        }
+
+        self.source.chars().nth(self.current as usize).unwrap()
+    }
+
+    fn peek_next(&self) -> char {
+        if self.current + 1 > self.source.len() as u32 {
+            return '\0';
+        }
+
+        self.source.chars().nth((self.current + 1) as usize).unwrap()
+    }
+
+    fn is_alpha(&self, c: char) -> bool {
+        (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
+    }
+
+    fn is_alpha_numeric(&self, c: char) -> bool {
+        self.is_alpha(c) || self.is_digit(c)
+    }
+
+    fn is_digit(&self, c: char) -> bool {
+        c >= '0' && c <= '9' 
+    }
+
     fn is_at_end(&self) -> bool {
-        return self.current >= self.source.len() as u32;
+        self.current >= self.source.len() as u32
     }
 
     fn advance(&mut self) -> char {
@@ -124,4 +223,25 @@ impl Scanner {
         })
     }
 
+    fn get_keyword(&self, word: &str) -> Option<TokenType> {
+        match word {
+            "and" => Some(TokenType::And),
+            "class" => Some(TokenType::Class),
+            "else" => Some(TokenType::Else),
+            "false" => Some(TokenType::False),
+            "for" => Some(TokenType::For),
+            "fun" => Some(TokenType::Fun),
+            "if" => Some(TokenType::If),
+            "nil" => Some(TokenType::Nil),
+            "or" => Some(TokenType::Or),
+            "print" => Some(TokenType::Print),
+            "return" => Some(TokenType::Return),
+            "super" => Some(TokenType::Super),
+            "this" => Some(TokenType::This),
+            "true" => Some(TokenType::True),
+            "var" => Some(TokenType::Var),
+            "while" => Some(TokenType::While),
+            _ => None
+        }
+    }
 }
